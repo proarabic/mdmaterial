af4abe194c0325ddb5d923ec767e543de93e1eb1
proarabic <30710501+proarabic@users.noreply.github.com>
2018-08-12 20:22:33 +0100

    Create md1.js

---
+++ md1.js
@@ -0,0 +1,1 @@
+function BlogMasTamvan(e,t){for(var g=document.getElementById(e),m=g.getElementsByTagName("img"),r=0;r<m.length;r++)m[r].src=m[r].src.replace(/\/s72\-c/,"/s"+t),m[r]}BlogMasTamvan("Blog1",1600);
